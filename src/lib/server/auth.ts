fb8d28485d4875c94dc19b6f90bbe0b9907b4f13
Kilan_vanloo <Kilanvanloo@gmail.com>
2025-01-24 21:52:25 -0300

    added dynamic url

--- src/lib/server/auth.ts
+++ src/lib/server/auth.ts
@@ -1,8 +1,6 @@
-import { SECRET_CLIENT_ID, SECRET_CLIENT_SECRET } from '$env/static/private';
+import { SECRET_CLIENT_ID, SECRET_CLIENT_SECRET, GOOGLE_REDIRECT_URL } from '$env/static/private';
 import { Google } from 'arctic';
-
-const redirectURL = 'http://localhost:5173/oauth';
 
 //export const oauth2Client = new OAuth2Client(SECRET_CLIENT_ID, SECRET_CLIENT_SECRET, redirectURL);
 
-export const google = new Google(SECRET_CLIENT_ID, SECRET_CLIENT_SECRET, redirectURL);
+export const google = new Google(SECRET_CLIENT_ID, SECRET_CLIENT_SECRET, GOOGLE_REDIRECT_URL);
